b9dcb1c7f8ca35f486c690318b2a16b43cccaad8
irekrog <irek.rog@mobilereality.pl>
2022-07-25 12:03:35 +0200

    fix: order

--- configs/react-native.js
+++ configs/react-native.js
@@ -5,9 +5,9 @@ module.exports = {
         'react-native/no-inline-styles': 'error',
         'react-native/no-color-literals': 'error',
         'react-native/no-single-element-style-arrays': 'error',
+        'react-native/split-platform-components': 'off',
         'react-native/no-raw-text': 'off',
         '@typescript-eslint/no-use-before-define': 'off',
         '@typescript-eslint/no-require-imports': 'off',
-        'react-native/split-platform-components': 'off',
     },
 };
