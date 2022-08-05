2186419d1709606e09f9eb8d403e09c7ad98b3d4
Mateusz Tkacz <mateusz.tkacz@mobilereality.pl>
2022-08-05 13:29:06 +0200

    feat: set custom config for unicorn consistent-function-scoping rule

--- configs/unicorn.js
+++ configs/unicorn.js
@@ -1,16 +1,21 @@
 module.exports = {
     extends: ['plugin:unicorn/recommended', 'prettier'],
     rules: {
-        'unicorn/catch-error-name': [
-            'warn',
-            {
-                name: 'err',
-            },
-        ],
         'unicorn/prevent-abbreviations': 'off',
         'unicorn/no-null': 'off',
         'unicorn/no-array-reduce': 'off',
         'unicorn/no-array-callback-reference': 'off',
         'unicorn/no-array-method-this-argument': 'off',
+
+        'unicorn/catch-error-name': [
+            'error',
+            {
+                name: 'err',
+            },
+        ],
+        'unicorn/consistent-function-scoping': [
+            'error',
+            { checkArrowFunctions: false },
+        ],
     },
 };
